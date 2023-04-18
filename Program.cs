d142d3995f17fe50bb74e2bbbff28cd4e5cfa0cb
Murilo Alves Zapiello <54750759805@info.local>
2023-04-18 17:20:38 -0300

    finalização atividade

--- Program.cs
+++ Program.cs
@@ -1,106 +1,127 @@
-﻿// Criar uma aplicação para uma agência de turismo, no qual deveremos registrar passagens aéreas com os seguintes dados: Nome do passageiro, Origem, Destino e Data do Voo de 5 passageiros.
+﻿// // Criar uma aplicação para uma agência de turismo, no qual deveremos registrar passagens aéreas com os seguintes dados: Nome do passageiro, Origem, Destino e Data do Voo de 5 passageiros.
 
-// Antes de entrar no sistema faça um esquema do qual o usuário só possa acessar o menu se a senha for igual à 123456. O sistema deve exibir um menu com as seguintes opções:
+// // Antes de entrar no sistema faça um esquema do qual o usuário só possa acessar o menu se a senha for igual à 123456.
 
-// 1- Cadastrar passagem
-// 2- Listar Passagens
-// 0- Sair
-// Observação :  Criar ao menos uma função (Efetuar Login).
+// // O sistema deve exibir um menu com as seguintes opções:
 
-// Ao cadastrar uma passagem ao final o sistema deverá perguntar se gostaria de cadastrar uma nova passagem caso contrário voltar ao menu anterior(S/N).
-
+// // 1- Cadastrar passagem
+// // 2- Listar Passagens
+// // 0- Sair
 
 
+// // Ao cadastrar uma passagem ao final, o sistema deverá perguntar se gostaria de cadastrar uma nova passagem caso contrário voltar ao menu anterior(S/N).
 
 
-Console.WriteLine($"Digite o seu nome e a senha que lhe foi enviada para entrar no sistema: ");
-string nome = "";
-int senha = 0;
+// //Algoritmo
+// //lista para armazenar nomes,origens,destinos e datas ok 
+// //validação de senha ok
+// entrada da senha pelo usuário ok
+// //menu de opções
+// //opção de cadastrar novas passagens
+
+//criando as funções
 
 
-while (senha != 123456 && nome == "")
+//estrutura de uma função
+
+//tipo tipoDado nomeFuncao(parâmetros ou argumentos)
 {
-    nome = Console.ReadLine();
-    senha = int.Parse(Console.ReadLine());
+    //Corpo da função
+}
 
-    if (senha == 123456)
+//função para verificar senha
+static bool Login(string senha)
+{
+    if (senha == "123456")
     {
-        Console.WriteLine($"Acesso concedido");
-        Console.WriteLine(@$"
-----------------------------------
-|      Passagens aéreas          |
-|        VAI COM DEUS            |
-| Digite:                        |
-|     1)Para cadastrar Mensagem  |
-|     2) Listar Passagem         |
-|     0) Sair                    |
-|  ®Viagens aéreas vai com Deus  |
-----------------------------------
-      ");
-        int nDigitado = int.Parse(Console.ReadLine());
-
-        for (int i = 0; i < 5; i++)
-        {
-            while (nDigitado != 1 || nDigitado != 0 || nDigitado != 2)
-            {
-                if (nDigitado == 1)
-                {
-                    cadastro();
-                }
-            }
-        }
-
-
+        return true;
     }
-
     else
     {
-        Console.WriteLine($"acesso negado, Digite a senha novamente");
-
+        Console.WriteLine($"Senha incorreta!");
+        return false;
     }
-
 }
 
-static void cadastro()
+//declarar as variáveis
+string[] nomes = new string[2];
+string[] origens = new string[2];
+string[] destinos = new string[2];
+string[] datas = new string[2];
+
+bool senhaValida;
+
+do
 {
-    string nomePass = "";
-    string orig = "";
-    string dest = "";
-    int dia = 0;
-    int mes = 0;
-    int ano = 0;
+    Console.WriteLine($"Informe a sua senha: ");
+    string senha = Console.ReadLine();
 
-    while (nomePass == "" && orig == "" && dest == "" && dia == 0 && mes == 0 && ano == 0)
+    senhaValida = Login(senha);
+
+} while (senhaValida != true);
+
+string opcao;
+
+do
+{
+    //criar menu de opções
+    Console.WriteLine($"Menu de opções");
+
+    Console.WriteLine(@$"
+Selecione um das opções
+[1] - Cadastrar
+[2] - Listar
+[0] - Sair
+");
+
+    opcao = Console.ReadLine();
+
+    string resposta;
+
+    switch (opcao)
     {
-        Console.WriteLine($"Digite o nome do passageiro: ");
-        nomePass = Console.ReadLine();
-        Console.WriteLine($"Digite da onde você ira partir: ");
-        orig = Console.ReadLine();
-        Console.WriteLine($"Digite qual é o seu destino: ");
-        dest = Console.ReadLine();
-        Console.WriteLine($"Digite o dia da viagem: ");
-        dia = int.Parse(Console.ReadLine());
-        Console.WriteLine($"mês: ");
-        mes = int.Parse(Console.ReadLine());
-        Console.WriteLine($"e ano: ");
-        ano = int.Parse(Console.ReadLine());
-        
+        case "1":
+            do
+            {
+                for (var i = 0; i < 2; i++)
+                {
+                    Console.WriteLine($"Informe o nome: ");
+                    nomes[i] = Console.ReadLine();
 
+                    Console.WriteLine($"Informe a origem: ");
+                    origens[i] = Console.ReadLine();
+
+                    Console.WriteLine($"Informe o destino: ");
+                    destinos[i] = Console.ReadLine();
+
+                    Console.WriteLine($"Informe a data: ");
+                    datas[i] = Console.ReadLine();
+                }
+
+                Console.WriteLine($"Quer cadastrar mais passagens: s/n");
+                resposta = Console.ReadLine().ToLower();
+
+            } while (resposta == "s");
+            break;
+        case "2":
+            for (var i = 0; i < 2; i++)
+            {
+                Console.WriteLine(@$"
+                *******************
+                Passagens - Bilhete
+
+                Nome: {nomes[i]}
+                Origem: {origens[i]}
+                Destino: {destinos[i]}
+                Data: {datas[i]}
+                ");
+            }
+            break;
+        case "0":
+            Console.WriteLine($"Fim do programa");
+            break;
+        default:
+            Console.WriteLine($"Opção inválida!");
+            break;
     }
-
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+} while (opcao != "0");
